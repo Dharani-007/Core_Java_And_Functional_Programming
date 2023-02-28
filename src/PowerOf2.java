ea2f363fc7d26d7b90b9bfd2df85d09f6af19d16
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-01 02:45:31 +0530

    Added : Power of two

---
+++ src/PowerOf2.java
@@ -0,0 +1,23 @@
+public class PowerOf2 {
+
+    public static void main(String args[] ) {
+// starts by parsing the integer
+        int n = Integer.parseInt(args[0]);
+
+        int i = 0;
+
+        int powerOfTwo = 1;
+
+        while (i <= n) {
+
+            System.out.println(i + " " + powerOfTwo);
+
+            powerOfTwo = 2 * powerOfTwo;
+
+            i = i + 1;
+
+        }
+
+    }
+
+}
