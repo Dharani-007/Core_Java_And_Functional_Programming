0b1af5486009a2a1514a9a1603564a050c6cd380
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-03-01 02:29:21 +0530

    Added : Flip Coin

--- src/FlipCoin.java
+++ src/FlipCoin.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class FlipCoin {
+public class FlipCoin {
+    public static void main(String args[])
+    {
+
+        if(Math.random() < 0.5 ) {
+            System.out.println("Heads");
+        }
+        else{
+            System.out.println("Tails");
+        }
+
+    }
 }
--- src/MaxNumber.java
+++ src/MaxNumber.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class MaxNumber {
+import java.util.Scanner;
+
+public class MaxNumber {
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter three numbers:");
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int num3 = input.nextInt();
+
+        if(num1 > num2 && num1 > num3)
+            System.out.println("The largest number is: " + num1);
+        else if(num2 > num1 && num2 > num3)
+            System.out.println("The largest number is: " + num2);
+        else
+            System.out.println("The largest number is: " + num3);
+    }
+
 }
+
